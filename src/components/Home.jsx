138421224f1c1f29efbb07953e9a06ad53d9c891
abdallamfathy <abdallamfathy@gmail.com>
2022-11-08 11:19:01 +0200

    Create files structure

---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>Clients</div>
+  )
+}
+
+export default Home
---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import About from "./About";
+import Contact from "./Contact";
+import Home from "./Home";
+import Projects from "./Projects";
+import Service from "./Service";
+import Navbar from "./Navbar";
+
+export { About, Contact, Home, Projects, Service, Navbar };
